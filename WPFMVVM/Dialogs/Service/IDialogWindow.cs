f4c7bebc633c7979b538ef8079da8cda797415b8
dh.cheon <34930947+Dolarge@users.noreply.github.com>
2022-05-18 09:39:50 +0900

    초기 커밋.

---
+++ WPFMVVM/Dialogs/Service/IDialogWindow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFMVVM.Dialogs.Service
+{
+    public interface IDialogWindow
+    {
+        bool? DialogReust { get; set; }
+        object DataContext { get; set; }
+        bool? ShowDialog();
+    }
+}
