598dd709072a2bd6e04754ffb6ae5992725d046f
dh.cheon <34930947+Dolarge@users.noreply.github.com>
2022-05-23 10:36:39 +0900

    feat : Menuitem left click

--- WPFMVVM/MVVM/ViewModel/MainViewModel.cs
+++ WPFMVVM/MVVM/ViewModel/MainViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using WPFMVVM.Dialogs.Service;
 
 namespace WPFMVVM.MVVM.ViewModel
@@ -13,10 +14,18 @@ namespace WPFMVVM.MVVM.ViewModel
     {
         private IDialogService _dialogService;
         public DelegateCommand TestCommand { get; private set; }
+        public DelegateCommand Menu1Command { get; private set; }
+
         public MainViewModel()
         {
             _dialogService = new DialogService();
             TestCommand = new DelegateCommand(ClickTest);
+            Menu1Command = new DelegateCommand(Menu1Click);
+        }
+
+        private void Menu1Click()
+        {
+            MessageBox.Show("Menu1", "Menu", MessageBoxButton.OK);
         }
 
         private void ClickTest()
